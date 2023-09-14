81694e95768ca6f6ed8037c71765ab3e713f354d
venkatcr592 <119779449+venkatcr592@users.noreply.github.com>
2023-09-14 22:45:25 +0530

    Create Task3.java

---
+++ Task3.java
@@ -0,0 +1,134 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Student {
+    private String name;
+    private int rollNumber;
+    private String grade;
+
+    public Student(String name, int rollNumber, String grade) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Roll Number: " + rollNumber + ", Grade: " + grade;
+    }
+}
+
+class StudentManagementSystem {
+    private List<Student> students;
+
+    public StudentManagementSystem() {
+        students = new ArrayList<>();
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public void removeStudent(int rollNumber) {
+        students.removeIf(student -> student.getRollNumber() == rollNumber);
+    }
+
+    public Student searchStudent(int rollNumber) {
+        for (Student student : students) {
+            if (student.getRollNumber() == rollNumber) {
+                return student;
+            }
+        }
+        return null;
+    }
+
+    public void displayAllStudents() {
+        for (Student student : students) {
+            System.out.println(student);
+        }
+    }
+
+    public List<Student> getAllStudents() {
+        return students;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        StudentManagementSystem sms = new StudentManagementSystem();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Student Management System");
+            System.out.println("1. Add Student");
+            System.out.println("2. Remove Student");
+            System.out.println("3. Search Student");
+            System.out.println("4. Display All Students");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine(); 
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter student name: ");
+                    String name = scanner.nextLine();
+                    System.out.print("Enter student roll number: ");
+                    int rollNumber = scanner.nextInt();
+                    scanner.nextLine(); 
+                    System.out.print("Enter student grade: ");
+                    String grade = scanner.nextLine();
+                    Student student = new Student(name, rollNumber, grade);
+                    sms.addStudent(student);
+                    break;
+
+                case 2:
+                    System.out.print("Enter student roll number to remove: ");
+                    int rollToRemove = scanner.nextInt();
+                    sms.removeStudent(rollToRemove);
+                    break;
+
+                case 3:
+                    System.out.print("Enter student roll number to search: ");
+                    int rollToSearch = scanner.nextInt();
+                    Student foundStudent = sms.searchStudent(rollToSearch);
+                    if (foundStudent != null) {
+                        System.out.println("Student found: " + foundStudent);
+                    } else {
+                        System.out.println("Student not found.");
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("All Students:");
+                    sms.displayAllStudents();
+                    break;
+
+                case 5:
+
+                    System.out.println("Exiting Student Management System.");
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
+}
+
